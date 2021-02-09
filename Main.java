2357511c79c1b3afb7f1e47bca309f71adcd261d
17Ashbringer76 <77210553+17Ashbringer76@users.noreply.github.com>
2021-02-09 23:01:51 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int payment = 1500;
+        long total;
+        long bonus;
+        if (payment >= 1000) {
+            bonus = payment / 100;
+            total = balance + payment;
+            System.out.println(bonus);
+            System.out.println(total);
+        }
+        if (payment <= 1000) {
+            bonus = 0;
+            total = balance + payment;
+            System.out.println(bonus);
+            System.out.println(total);
+        }
+    }
+}
+
